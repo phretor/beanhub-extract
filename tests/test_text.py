347ae12dc810bc7879489900fae73a031766d5d6
Fang-Pen Lin <hello@fangpenlin.com>
2025-12-20 12:09:30 -0800

    Add as text wrapper

---
+++ tests/test_text.py
@@ -0,0 +1,19 @@
+import pathlib
+
+import pytest
+
+from beanhub_extract.text import as_text
+
+
+@pytest.mark.parametrize("encoding", ["utf8", "utf16", "cp950"])
+def test_as_text_with_binary_file(tmp_path: pathlib.Path, encoding: str):
+    bin_file = tmp_path / "bin.txt"
+    text = "hello there, 你好"
+    bin_file.write_text(text, encoding=encoding)
+
+    with bin_file.open("rb") as fo:
+        with as_text(fo, encoding=encoding) as text_file:
+            assert text_file.read() == text
+        # ensure that the file is not closed
+        fo.seek(0)
+        assert fo.read() == text.encode(encoding)
